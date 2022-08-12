774d09fd04c9f8b1b5878dcd8f0c419934df8580
A0001Mohamed <111150669+A0001Mohamed@users.noreply.github.com>
2022-08-13 01:09:36 +0530

    Add files via upload

---
+++ trail.js
@@ -0,0 +1,42 @@
+// const name={
+//     names:"mohamed",
+//     age:25,
+//     "hobbies":"cricket",
+//     sayName:function(){
+//         console.log(this.names)
+
+//     }
+// }
+
+// name.address="peter"
+// //delete name.address
+
+// console.log(name.names)
+// console.log(name['age'])
+// console.log(name['hobbies'])
+// name.sayName()
+// //console.log(name)
+
+
+const arr=[1,2,3,'ameen']
+const ar=arr.filter(myname)
+console.log(ar)
+//const newArr=arr.map(my)
+//const num=[6,5,7]
+
+function myname(arr){
+    return arr >=2;
+}
+
+// function my(num){
+//     return num+1;
+// }
+// arr.unshift(0)
+// arr.push(5)
+// arr.pop()
+// arr.shift()
+// for(const item of arr){
+//     console.log(item)
+// }
+
+// console.log(newArr)
